0f4c1e851d3cbffaaa360c7bf31ecf963daf623e
Brandon Lee <vitualizz@gmail.com>
2018-02-03 20:38:49 -0500

    Generador de cuadrados

---
+++ cuadrado.rb
@@ -0,0 +1,15 @@
+puts "Ingrese algun caracter"
+c = gets.chomp
+puts "Ingrese la altura del cuadrado"
+n = gets.chomp.to_i.abs
+puts "Ingrese el ancho del cuadrado"
+a = gets.chomp.to_i.abs
+puts "Cuadrado ven ami!!! BOOOOOOOOMMMMMMM!!!"
+all = 0..n
+for i in all
+  if all.first==i or all.last==i 
+    puts " " +c * a
+  else
+    puts c + (" " * a) + c 
+  end
+end
