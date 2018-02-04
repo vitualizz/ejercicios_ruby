0b61f7902ac060ef7314d89ff8940c28fd43df25
Brandon Lee <vitualizz@gmail.com>
2018-02-04 11:23:37 -0500

    Calculadora de porcentajes

---
+++ porcentaje.rb
@@ -0,0 +1,6 @@
+puts "Ingrese una variable para sacarle el porcentaje:"
+var = gets.chomp.to_i
+puts "Ingrese el porcentaje:"
+por = gets.chomp.to_i
+puts "El porcentaje de este numero es:"
+puts (var*por)/100
