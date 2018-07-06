093f1a74450b4e42c26d0cc77986620e9270c299
Lee Palacios <vitualizz@gmail.com>
2018-07-06 17:20:32 -0500

    test conflict

--- cuadrado.rb
+++ cuadrado.rb
@@ -4,7 +4,7 @@ puts "Ingrese la altura del cuadrado"
 n = gets.chomp.to_i.abs
 puts "Ingrese el ancho del cuadrado"
 a = gets.chomp.to_i.abs
-puts "Cuadrado ven ami!!! BOOOOOOOOMMMMMMM!!!"
+puts "Cuadrado ven ami!!! BOOMMM!!!"
 all = 0..n
 for i in all
   if all.first==i or all.last==i 
