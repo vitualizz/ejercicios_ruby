7e23b4c5ab9deed8fae3c505efde3ef5e25989b4
Lee Palacios <vitualizz@gmail.com>
2018-10-09 23:10:35 -0500

    cambio 4

--- cuadrado.rb
+++ cuadrado.rb
@@ -4,7 +4,7 @@ puts "Altura del cuadrado"
 n = gets.chomp.to_i.abs
 puts "Ingrese el ancho del cuadrado"
 a = gets.chomp.to_i.abs
-puts "Cuadrado ven ami!!! BOOMMM!!!"
+puts "Cuadrado ven ami!!!"
 all = 0..n
 for i in all
   if all.first==i or all.last==i 
